a54d71f360e20822fa964520a3d36707ffbc5987
Sukuna Shinmyoumaru <shs3128ym@gmail.com>
2022-04-01 21:32:33 +0900

    feat: 테스트 내일 돌릴 거임

---
+++ src/main/kotlin/com/lianserver/skinscroll/Main.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2022 AlphaGot
+ *
+ *  Licensed under the General Public License, Version 3.0. (https://opensource.org/licenses/gpl-3.0/)
+ */
+
+package com.lianserver.skinscroll
+
+import com.github.stefvanschie.inventoryframework.gui.GuiItem
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui
+import com.github.stefvanschie.inventoryframework.pane.StaticPane
+import com.lianserver.skinscroll.interfaces.PrefixedTextInterface
+import io.github.monun.kommand.Kommand.Companion.register
+import io.github.monun.kommand.getValue
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+/***
+ * @author AlphaGot
+ */
+
+class Main : JavaPlugin(), Listener, PrefixedTextInterface {
+
+    companion object {
+        lateinit var instance: Main
+            private set
+    }
+
+    lateinit var skinItems: YamlConfiguration
+
+    override fun onEnable() {
+        instance = this
+
+        skinItems = YamlConfiguration.loadConfiguration(File(dataFolder, "db.yml"))
+
+        server.pluginManager.registerEvents(this, this)
+
+        register(this, "getskinitem"){
+            requires { player.isOp }
+            executes { player.sendMessage(adminText("스킨 아이디를 입력해주세요.")) }
+            then("id" to int()){
+                executes {
+                    val id: Int by it
+
+                    player.inventory.addItem(skinItems.getItemStack(id.toString()) ?: ItemStack(Material.BROWN_WOOL))
+                    player.sendMessage(adminText("아이템을 지급했습니다."))
+                }
+            }
+        }
+        register(this, "regskinitem"){
+            requires { player.isOp }
+            executes { player.sendMessage(adminText("스킨 아이디를 입력해주세요.")) }
+            then("id" to int()){
+                executes {
+                    val id: Int by it
+
+                    if(player.inventory.itemInMainHand.type == Material.ENCHANTED_BOOK && player.inventory.itemInMainHand.hasItemMeta()){
+                        skinItems[id.toString()] = player.inventory.itemInMainHand
+                        player.sendMessage(adminText("아이템을 등록했습니다."))
+                    }
+                    else {
+                        player.sendMessage(adminText("스킨 아이템이 아닙니다."))
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDisable() {
+        logger.info(adminTextS("아새기들 일 안하네"))
+        skinItems.save(File(dataFolder, "db.yml"))
+    }
+
+    @EventHandler
+    fun onUseEffectBook(e: PlayerInteractEvent){
+        if(e.hasItem()){
+            if(e.item!!.type == Material.ENCHANTED_BOOK){
+                if(e.item!!.hasItemMeta()){
+                    if(e.item!!.itemMeta.hasLore()){
+                        val c = PlainTextComponentSerializer.plainText().serialize(e.item!!.itemMeta.lore()!![0]!!)
+
+                        if(c.contains("(id=")){
+                            val id = c.split("=")[1].replace(")", "")
+
+                            val invSelItem = ChestGui(4, "스킨을 적용할 아이템 선택")
+
+                            val p = StaticPane(0, 0, 9, 4)
+
+                            val xy = Pair(0, 0)
+
+                            e.player.inventory.forEach {
+                                p.addItem(
+                                    GuiItem(
+                                        it
+                                    ){ ev: InventoryClickEvent ->
+                                        ev.isCancelled = true
+
+                                        val m = it.itemMeta
+
+                                        m.setCustomModelData(id.toIntOrNull())
+                                        it.itemMeta = m
+
+                                        e.player.sendMessage(userText("").append(it.displayName()).append(text("아이템에 스킨을 적용했습니다.")))
+                                        e.item!!.subtract(1)
+                                    },
+                                    xy.first,
+                                    xy.second
+                                )
+                            }
+
+                            invSelItem.addPane(p)
+                            invSelItem.update()
+
+                            invSelItem.show(e.player)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/lianserver/skinscroll/interfaces/PrefixedTextInterface.kt
@@ -0,0 +1,25 @@
+package com.lianserver.skinscroll.interfaces
+
+import com.lianserver.skinscroll.Main
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+
+interface PrefixedTextInterface {
+    private fun getInstance(): Main = Main.instance
+
+    fun clanText(s: String): TextComponent =
+        Component.text("${getInstance().config.getString("clan_prefix")} $s")
+    fun clanTextS(s: String): String = "${getInstance().config.getString("clan_prefix")} $s"
+
+    fun adminText(s: String): TextComponent =
+        Component.text("${getInstance().config.getString("admin_prefix")} $s")
+    fun adminTextS(s: String): String = "${getInstance().config.getString("admin_prefix")} $s"
+
+    fun countryText(s: String): TextComponent =
+        Component.text("${getInstance().config.getString("country_prefix")} $s")
+    fun countryTextS(s: String): String = "${getInstance().config.getString("country_prefix")} $s"
+
+    fun userText(s: String): TextComponent =
+        Component.text("${getInstance().config.getString("user_prefix")} $s")
+    fun userTextS(s: String): String = "${getInstance().config.getString("user_prefix")} $s"
+}
